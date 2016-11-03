f1cfedf6e118077d9924c1573486d43c9dbbb4f5
shanhong cheng <shanhong.cheng@utah.edu>
2016-11-03 14:22:35 -0600

    test all

--- bin/test_all.sh
+++ bin/test_all.sh
@@ -2,5 +2,6 @@ export RAILS_ENV=test
 mkdir -p public/system/documents
 cp .env .env.back
 cp dotenv.example .env
-bundle exec rspec
+today=`date +%Y-%m-%d-%H-%M-%S`
+bundle exec rspec --color --format documentation --out tmp/${today}-test-results
 cp .env.back .env
