795f3edd7dd6ad42ed61167fa0e3474dfaf5daac
shanhong cheng <shanhong.cheng@utah.edu>
2016-11-10 13:28:30 -0700

    test all script

--- bin/test_all.sh
+++ bin/test_all.sh
@@ -1,7 +1,15 @@
+#!/usr/bin/env bash
+
+# load rvm ruby
+source /Users/shanhong/.rvm/environments/ruby-2.1.5
+
 export RAILS_ENV=test
+# cd to project directory
+cd /Volumes/HD2/projects/uofu/sparc-fulfillment
 mkdir -p public/system/documents
 cp .env .env.back
 cp dotenv.example .env
 today=`date +%Y-%m-%d-%H-%M-%S`
-bundle exec rspec --color --format documentation --out tmp/${today}-test-results
+echo "bundle exec rspec --color --format documentation --out tmp/${today}-test-results"
+rvm 2.1.5 do bundle exec rspec --color --format documentation --out tmp/${today}-test-results
 cp .env.back .env
